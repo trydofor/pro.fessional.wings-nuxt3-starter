c2e5b2c1e4c66dc1bfdc7b3e2e9fc063633b90de
trydofor <trydofor@gmail.com>
2023-09-19 17:14:19 +0800

    006.add app.config

---
+++ src/app.config.ts
@@ -0,0 +1,11 @@
+import { defineAppConfig } from 'nuxt/app';
+
+export default defineAppConfig({
+  title: 'Wings Nuxt3 Starter',
+  theme: {
+    dark: true,
+    colors: {
+      primary: '#ff0000',
+    },
+  },
+});
